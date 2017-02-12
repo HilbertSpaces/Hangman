fe7eff465364370ed0facf1831c05430aea55139
HilbertSpaces <Jktecon@gmail.com>
2017-02-12 18:47:46 -0500

    server tests

--- hangServer.py
+++ hangServer.py
@@ -1,75 +1,78 @@
-from flask import Flask, render_template, session, jsonify,redirect,abort
-from random import randint
+from flask import Flask, render_template, session, jsonify,redirect,abort,json
+from random import choice
 app= Flask(__name__)
 #set secret key for sessions
 app.secret_key='F33z1ibmnouuIcxr3r4j4clevelslove'
 with open('words.txt') as f:
-	content=f.readlines()
-wordArr=[x.rstrip() for x in content]
+	content = f.readlines()
+wordArr = [x.rstrip() for x in content]
 
 def genWord(wordArr):
-    wordArrLen=len(wordArr)-1
-    word_index=randint(0,wordArrLen)
-    word=wordArr[word_index]
+    word=choice(wordArr)
     if '\'' in word:
-        genWord()
+        return genWord(wordArr)
     wordLen=len(word)
     return word,wordLen
 
 def persistData():
-    session['wins']=0
-    session['totGames']=0
+    session['wins'] = 0
+    session['totGames'] = 0
 
 def sessionData():
-    session['correct']=0
-    session['guesses']=0
-    session['finished']=False
-    session['totalCorrect']=0
+    session['correct'] = 0
+    session['guesses'] = 0
+    session['finished'] = False
+    session['totalCorrect'] = 0
 
 def sessionWord():
-    session['total']=genWord(wordArr)
-    session['word']=session['total'][0]
-    session['wordLen']=session['total'][1]
+    session['total'] = genWord(wordArr)
+    session['word'] = session['total'][0]
+    session['wordLen'] = session['total'][1]
 
 @app.route('/')
-@app.route('/<cover>',methods=['GET'])
+@app.route('/<cover>',methods = ['GET'])
 def home(cover=None):
-    if cover=='sessionCover':
+    if cover == 'sessionCover':
         sessionWord()
         sessionData()
-        return render_template('index.html',my_len=session['wordLen'])
+        return render_template('index.html',my_len = session['wordLen'])
     elif cover==None:
         persistData()
         sessionData()
         sessionWord()
-        return render_template('index.html',my_len=session['wordLen'])
+        return render_template('index.html',my_len = session['wordLen'])
     else:
         abort(404)
 
 @app.route('/guess/<guess>')
 def guess(guess):
     guess=chr(int(guess)+96)
-    locations=[]
+    locations = []
     inside=False
-    session['guesses']+=1
+    session['guesses'] += 1
     correctOnce=False
     for i in range(session['wordLen']):
-        if guess==session['word'][i]:
-            inside=True
-            session['totalCorrect']+=1
+        if guess == session['word'][i]:
+            inside = True
+            session['totalCorrect'] += 1
             if not correctOnce:
-                session['correct']+=1
-                correctOnce=True
+                session['correct'] += 1
+                correctOnce = True
             locations.append(i)
-    if session['totalCorrect']==session['wordLen']:
-        session['finished']=True
-        session['wins']+=1
-        session['totGames']+=1
-    elif session['guesses']-session['correct']==10:
-        session['finished']=True
-        session['totGames']+=1
-    sessionJSON={'inside':inside,'locations':locations,'correct':session['correct'],
-            'guesses':session['guesses'],'finished':session['finished'],
-            'wins':session['wins'],'totGames':session['totGames']}
+    if session['totalCorrect'] == session['wordLen']:
+        session['finished'] = True
+        session['wins'] += 1
+        session['totGames'] += 1
+    elif session['guesses']-session['correct'] == 10:
+        session['finished'] = True
+        session['totGames'] += 1
+    sessionJSON = {'inside':inside,'locations':locations,
+            'correct':session['correct'],
+            'guesses':session['guesses'],
+            'finished':session['finished'],
+            'wins':session['wins'],
+            'totGames':session['totGames']}
     return jsonify(sessionJSON)
-app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ testServer.py
@@ -0,0 +1,77 @@
+from hangServer import *
+import unittest
+import string
+
+PRESERVE_CONTEXT_ON_EXCEPTION = False
+
+class ServerTests(unittest.TestCase):
+##########################################################
+    #test the word generation function to make sure
+    #words with apostrophes are never used
+    def test_word_generation(self):
+        word_array = ['don\'t','won\'t','hello']
+        word = genWord(word_array)
+        for i in range(1000):
+            self.assertEqual(word,('hello',5))
+
+##########################################################
+# test routes for clean get requests 
+
+    def test_routes(self):
+        with app.test_client() as c:
+            response = c.get('/')
+            self.assertEqual(response.status_code,200)
+            response=c.get('/sessionCover')
+            self.assertEqual(response.status_code,200)
+            #guess A
+            response=c.get('/guess/1')
+            self.assertEqual(response.status_code,200)
+
+##########################################################
+#test game logic
+
+    def test_logic(self):
+        with app.test_client() as c:
+            #get home route to create the session objects
+            response = c.get('/')
+            '''check session objects and set our word to hello
+            for effective testing
+            '''
+            with c.session_transaction() as session:
+                session['total'] = genWord(['hello'])
+                session['word'] = session['total'][0]
+                session['wordLen'] = session['total'][1]
+                self.assertEqual(session['correct'],0)
+                self.assertEqual(session['wins'],0)
+                self.assertEqual(session['guesses'],0)
+
+            #!!!make some guesses against hello!!!
+            #guess 'e' (correct)
+            response = c.get('/guess/5')
+            with c.session_transaction() as session:
+                self.assertEqual(session['guesses'],1)
+                self.assertEqual(session['correct'],1)
+
+            #guess 'a' (wrong)
+            response = c.get('/guess/1')
+            with c.session_transaction() as session:
+                self.assertEqual(session['guesses'],2)
+                self.assertEqual(session['correct'],1)
+##########################################################
+# test one full json response, guess 'l'
+
+            response = c.get('/guess/12')
+            data = json.loads(response.data)
+            self.assertEqual(data, dict(inside=True,
+                locations = [2,3],correct = 2,guesses = 3,
+                finished = False,wins = 0,totGames = 0
+                ))
+##########################################################
+
+            response = c.get('/guess/8')
+            response = c.get('/guess/15')
+            with c.session_transaction() as session:
+                self.assertEqual(session['wins'],1)
+                self.assertEqual(session['totGames'],1)
+
+unittest.main()
